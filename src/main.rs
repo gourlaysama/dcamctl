ebe50527ae2ea5e83aec7f71d3f24166c3b1506a
Antoine Gourlay <antoine@gourlay.fr>
2021-04-11 17:10:19 +0200

    move to gstreamer-rs dependency

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,16 @@
-use std::path::Path;
 use std::process::Command;
+use std::{
+    path::Path,
+    sync::mpsc::{self, Receiver},
+    thread,
+};
 
 use anyhow::*;
 use dcam::cli::ProgramOptions;
 use env_logger::{Builder, Env};
+use gstreamer::prelude::*;
 use log::*;
+use mpsc::TryRecvError;
 use structopt::StructOpt;
 
 type ReturnCode = i32;
@@ -55,39 +61,52 @@ fn run(options: ProgramOptions) -> Result<ReturnCode> {
         "video/x-raw,format=YUY2,width={},height={}",
         options.resolution.width, options.resolution.height
     );
-    let mut child = Command::new("gst-launch-1.0")
-        .args(&["-e", "-vt", "--gst-plugin-spew"])
-        .args(&[
-            "souphttpsrc",
-            "location=http://127.0.0.1:8080/videofeed",
-            "do-timestamp=true",
-            "is-live=true",
-            "!",
-            "queue",
-            "!",
-            "multipartdemux",
-            "!",
-            "decodebin",
-            "!",
-            "videoconvert",
-            "!",
-            "videoscale",
-            "!",
-            &caps,
-            "!",
-            "v4l2sink",
-            &device,
-            "sync=true",
-        ])
-        .spawn()?;
+
+    gstreamer::init()?;
+
+    let pipeline = gstreamer::parse_launch(
+        &format!("souphttpsrc location=http://127.0.0.1:{}/videofeed do-timestamp=true is-live=true ! queue ! multipartdemux ! decodebin ! videoconvert ! videoscale ! {} ! v4l2sink {} sync=true", options.port, caps, device))?;
+
+    pipeline.set_state(gstreamer::State::Playing)?;
 
     println!("Press <Enter> to disconnect the webcam.");
-    let mut _o = String::new();
-    std::io::stdin().read_line(&mut _o)?;
+    let stdin_channel = watch_stdin();
 
-    child.kill()?;
+    let bus = match pipeline.get_bus() {
+        Some(b) => b,
+        None => bail!("No bus for gstreamer pipeline"),
+    };
+    loop {
+        let msg = bus.timed_pop(100 * gstreamer::MSECOND);
+        if let Some(msg) = msg {
+            use gstreamer::MessageView;
 
-    // add forwarding rule
+            match msg.view() {
+                MessageView::Eos(..) => break,
+                MessageView::Error(err) => {
+                    error!(
+                        "Error from {:?}: {} ({:?})",
+                        err.get_src().map(|s| s.get_path_string()),
+                        err.get_error(),
+                        err.get_debug()
+                    );
+                    break;
+                }
+                _ => {}
+            }
+        }
+
+        match stdin_channel.try_recv() {
+            Ok(_) => break,
+            Err(TryRecvError::Empty) => {}
+            Err(a) => return Err(anyhow!("Internal threading error").context(a)),
+        }
+    }
+
+    // Shutdown pipeline
+    pipeline.set_state(gstreamer::State::Null)?;
+
+    // remove forwarding rule
     let res = Command::new("adb")
         .arg("forward")
         .arg("--remove")
@@ -98,6 +117,17 @@ fn run(options: ProgramOptions) -> Result<ReturnCode> {
     }
 
     Ok(0)
+}
+
+fn watch_stdin() -> Receiver<()> {
+    let (tx, rx) = mpsc::channel::<()>();
+    thread::spawn(move || loop {
+        // ignore line content
+        let mut buffer = String::new();
+        std::io::stdin().read_line(&mut buffer).unwrap();
+        tx.send(()).unwrap();
+    });
+    rx
 }
 
 fn check_kernel_module() -> Result<()> {

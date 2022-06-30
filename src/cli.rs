730844d6f943e89f942670ef1931157f0acd813f
Antoine Gourlay <antoine@gourlay.fr>
2022-06-30 15:14:16 +0200

    build: move to clap v3

--- src/cli.rs
+++ src/cli.rs
@@ -1,19 +1,20 @@
 use std::path::PathBuf;
 
 use log::LevelFilter;
-use structopt::StructOpt;
 
-#[derive(StructOpt, Debug)]
-#[structopt(
+#[derive(clap::Parser, Debug)]
+#[clap(
     about = "Use android device as webcam with v4l2loopback",
-    setting = structopt::clap::AppSettings::DisableVersion,
+    setting = clap::AppSettings::NoAutoVersion,
+    mut_arg("help", |h| h.help_heading("INFO")),
+    mut_arg("version", |h| h.help_heading("INFO")),
 )]
 pub struct ProgramOptions {
     /// Port to forward between the device and localhost.
     ///
     /// The port on on the device with this value will be forwarded to the same port on localhost.
     /// [default: 8080]
-    #[structopt(long, short)]
+    #[clap(long, short)]
     pub port: Option<u16>,
 
     /// v4l2loopback video device to use.
@@ -21,54 +22,57 @@ pub struct ProgramOptions {
     /// This device must be one expose by the v4l2loopback kernel module. Check the devices under /dev/video* with
     /// `v4l2-ctl -d /dev/videoX -D` for the correct one.
     /// [default: /dev/video0]
-    #[structopt(long, short)]
+    #[clap(long, short)]
     pub device: Option<String>,
 
     /// Output resolution to use.
     ///
     /// The video feed will be resized to this value if needed.
     /// [default: auto]
-    #[structopt(long, short)]
+    #[clap(long, short)]
     pub resolution: Option<String>,
 
     /// Use the given configuration file instead of the default.
     ///
     /// By default, dcamctl looks for a configuration file in "$XDG_CONFIG_HOME/dcamctl/config.yml"
     /// or "$HOME/.config/dcamctl/config.yml".
-    #[structopt(long)]
+    #[clap(long, value_name = "FILE")]
     pub config: Option<PathBuf>,
 
     /// Disable audio support.
     ///
     /// Do not setup audio forwarding or interact at all with the audio system.
-    #[structopt(long, short)]
+    #[clap(long, short, help_heading = "FLAGS")]
     pub no_audio: bool,
 
     /// Disable echo canceling.
-    #[structopt(long, short = "C")]
+    #[clap(long, short = 'C', help_heading = "FLAGS")]
     pub no_echo_cancel: bool,
 
     /// Flip method used to mirror the video.
     ///
     /// Defaults to none.
-    #[structopt(long, short, possible_values(&["horizontal", "vertical", "none"]))]
+    #[clap(long, short, possible_values(&["horizontal", "vertical", "none"]), value_name = "METHOD")]
     pub flip: Option<String>,
 
-    /// Prints version information.
-    #[structopt(short = "V", long = "version")]
-    pub version: bool,
-
     /// Pass for more log output.
-    #[structopt(long, short, global = true, parse(from_occurrences))]
-    verbose: i8,
-
-    /// Pass for less log output.
-    #[structopt(
+    #[clap(
         long,
         short,
         global = true,
         parse(from_occurrences),
-        conflicts_with = "verbose"
+        help_heading = "FLAGS"
+    )]
+    verbose: i8,
+
+    /// Pass for less log output.
+    #[clap(
+        long,
+        short,
+        global = true,
+        parse(from_occurrences),
+        conflicts_with = "verbose",
+        help_heading = "FLAGS"
     )]
     quiet: i8,
 }
